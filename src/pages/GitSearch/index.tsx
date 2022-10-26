3b9fd9e903d5092f1774ed19ec504f01736dbcbe
Vanderlei Bueno <vanderlei.llbueno@hotmail.com>
2022-10-26 15:28:53 -0300

    handling form events and data

--- src/pages/GitSearch/index.tsx
+++ src/pages/GitSearch/index.tsx
@@ -1,19 +1,53 @@
 import ResultCard from 'components/ResultCard';
+import { useState } from 'react';
 import './styles.css';
 
+type FormData = {
+  git: string;
+  test: string;
+}
+
 const GitSearch = () => {
+
+  const [formData, setFormData] = useState<FormData>({
+    git: '',
+    test: ''
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData( { ...formData, [name]:value } )
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <>
       <div className="git-search-container">
         <div className="container search-container">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-container">
               <h1>Encontre um perfil Github</h1>
               <input
                 type="text"
+                name="git"
+                value={formData.git}
                 className="search-input"
                 placeholder="usuário Github"
-                onChange={() => {}}
+                onChange={handleChange}
+              />
+              <input
+                type="text"
+                name="test"
+                value={formData.test}
+                className="search-input"
+                placeholder="teste"
+                onChange={handleChange}
               />
               <button type="submit" className="btn btn-primary search-button">
                 Encontrar
@@ -24,7 +58,12 @@ const GitSearch = () => {
       </div>
       <div className="img-result-container">
         <div className="container result-container">
-          <ResultCard title="perfil" description="seguidores" test="localidade" name="nome" />
+          <ResultCard
+            title="perfil"
+            description="seguidores"
+            test="localidade"
+            name="nome"
+          />
         </div>
       </div>
     </>
