500c3f25eb5582f322467cf891935f374eb14cba
Vanderlei Bueno <vanderlei.llbueno@hotmail.com>
2022-10-28 11:07:56 -0300

    Integrating with github API

--- src/components/ResultCard/index.tsx
+++ src/components/ResultCard/index.tsx
@@ -3,23 +3,14 @@ import './styles.css';
 type Props = {
   title: string;
   description: string;
-  test: string;
-  name: string;
 };
 
-const ResultCard = ({ title, description, test, name }: Props) => {
+const ResultCard = ({ title, description }: Props) => {
   return (
     <>
       <div className="img-result-container2">
-        <div className="img-container2">
-          <h1>IMAGEM</h1>
-        </div>
         <div className="result-container2">
-          <h1>Informações</h1>
-          <h3 className="result-title">{title}</h3>
-          <h3 className="result-title">{description}</h3>
-          <h3 className="result-title">{test}</h3>
-          <h3 className="result-title">{name}</h3>
+          <h3 className="result-title">{title}{description}</h3>
         </div>
       </div>
     </>
--- src/pages/GitSearch/index.tsx
+++ src/pages/GitSearch/index.tsx
@@ -1,29 +1,45 @@
+import axios from 'axios';
 import ResultCard from 'components/ResultCard';
 import { useState } from 'react';
 import './styles.css';
 
 type FormData = {
   git: string;
-  test: string;
-}
+};
+
+type GitData = {
+  avatar_url: string;
+  url: string;
+  name: string;
+  location: string;
+  followers: string;
+};
 
 const GitSearch = () => {
+  const [gitdata, setGitData] = useState<GitData>();
 
   const [formData, setFormData] = useState<FormData>({
     git: '',
-    test: ''
   });
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const name = event.target.name;
     const value = event.target.value;
 
-    setFormData( { ...formData, [name]:value } )
+    setFormData({ ...formData, [name]: value });
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(formData);
+
+    axios
+      .get(`https://api.github.com/users/${formData.git}`)
+      .then((response) => {
+        setGitData(response.data);
+      })
+      .catch((error) => {
+        setGitData(undefined);
+      });
   };
 
   return (
@@ -41,14 +57,6 @@ const GitSearch = () => {
                 placeholder="usuário Github"
                 onChange={handleChange}
               />
-              <input
-                type="text"
-                name="test"
-                value={formData.test}
-                className="search-input"
-                placeholder="teste"
-                onChange={handleChange}
-              />
               <button type="submit" className="btn btn-primary search-button">
                 Encontrar
               </button>
@@ -56,14 +64,33 @@ const GitSearch = () => {
           </form>
         </div>
       </div>
+
       <div className="img-result-container">
         <div className="container result-container">
-          <ResultCard
-            title="perfil"
-            description="seguidores"
-            test="localidade"
-            name="nome"
-          />
+          {gitdata && (
+            <>
+              <div className="box-container">
+                <div className="img-container">
+                  <img src={gitdata.avatar_url} alt="Avatar" />
+                </div>
+                <div>
+                  <div className="info-container">
+                    <h3>Informações</h3>
+                  </div>
+                  <ResultCard title="Perfil: " description={gitdata.url} />
+                  <ResultCard
+                    title="Seguidores: "
+                    description={gitdata.followers}
+                  />
+                  <ResultCard
+                    title="Localidade: "
+                    description={gitdata.location}
+                  />
+                  <ResultCard title="Nome: " description={gitdata.name} />
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
